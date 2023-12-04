5433edd34ba6b373df0f68425709775afae02f0e
Ritobusk <tjornlyng@gmail.com>
2023-12-04 10:49:35 +0100

    day 4 2

--- 4/main.py
+++ 4/main.py
@@ -1,17 +1,21 @@
-
+f = open("in.in").readlines()
+total2 = [1] * len(f)
 total = 0
 
-for line in open("in.in"):
+for i, line in enumerate(f):
     line = line.split(': ')[1].strip().split(' | ')
-    winning_nums = -1
+    winning_nums = 0
     winners = set(line[0].split())
     my_nums = line[1].split()
-    print(winners, my_nums)
     for num in my_nums:
         if num in winners:
             winning_nums += 1
 
-    if winning_nums > -1:
-        total += pow(2, winning_nums)
+    boards = total2[i]
+        
+    if winning_nums > 0:
+        for j in range(1, winning_nums + 1):
+            if j + i < len(f):
+                total2[i+j] += boards
 
-print(total)
+print(sum(total2))
